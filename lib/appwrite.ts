e3ec70e0cb514f7dd0625000f31c949b0425f62c
Abhinav Gusain <232528803+abhinavvgusainn@users.noreply.github.com>
2026-05-30 15:40:10 +0530

    use env for appwrite endpoint

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,7 @@
 import { Client, Databases } from "appwrite";
 
 const client = new Client()
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
   .setProject(
     process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!
   );
